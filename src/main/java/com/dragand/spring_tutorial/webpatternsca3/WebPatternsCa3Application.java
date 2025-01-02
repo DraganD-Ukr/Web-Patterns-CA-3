c3118a20ecd71d899f8a3f1d86f75e3e761d8869
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-01-02 17:04:22 +0000

    Initial commit

---
+++ src/main/java/com/dragand/spring_tutorial/webpatternsca3/WebPatternsCa3Application.java
@@ -0,0 +1,13 @@
+package com.dragand.spring_tutorial.webpatternsca3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPatternsCa3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPatternsCa3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragand/spring_tutorial/webpatternsca3/WebPatternsCa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragand.spring_tutorial.webpatternsca3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPatternsCa3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
