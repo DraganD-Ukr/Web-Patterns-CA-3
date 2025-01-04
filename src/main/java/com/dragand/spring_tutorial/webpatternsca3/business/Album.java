6b18b8b39fb8144260b0a24a2b6637edd349fef6
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-01-04 15:27:12 +0000

    Added Album model

---
+++ src/main/java/com/dragand/spring_tutorial/webpatternsca3/business/Album.java
@@ -0,0 +1,32 @@
+package com.dragand.spring_tutorial.webpatternsca3.business;
+
+import lombok.*;
+
+import java.util.Date;
+
+/**
+ * @author: Dmytro Drahan
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Album {
+
+    @EqualsAndHashCode.Include
+    private Integer albumId;
+    private String title;
+    private Integer artistId;
+    private Date releaseDate;
+
+    @Override
+    public String toString() {
+        return
+                "Id=" + albumId +
+                        ", title='" + title + '\'' +
+                        ", artistId=" + artistId +
+                        ", Release date=" + releaseDate;
+    }
+}
