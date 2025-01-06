d41b8756894a0b3aed9724ed5d0a5fcbbf7c4287
Aloysius <aloysiuspacheco2003@gmail.com>
2025-01-06 20:33:51 +0000

    addition: comlpleted base prep on DAO interface. All needed methods for search included.

--- src/main/java/com/dragand/spring_tutorial/webpatternsca3/persistence/SongDAO.java
+++ src/main/java/com/dragand/spring_tutorial/webpatternsca3/persistence/SongDAO.java
@@ -1,4 +1,35 @@
 package com.dragand.spring_tutorial.webpatternsca3.persistence;
 
+import com.dragand.spring_tutorial.webpatternsca3.business.Song;
+
+import java.util.List;
+
+/**
+ * Song DAO interface used to define the methods that will be implemented in the SongDAOImpl class
+ *
+ * @author Aloysius Wilfred Pacheco D00253302
+ */
 public interface SongDAO {
+    /**
+     * Gets A song by its title from the database
+     * @param title The title of the song to find
+     * @return The {@link Song} object if found, otherwise {@code null}
+     */
+
+    //Search Querry methods
+
+    public Song findSongByTitle(String title);
+
+    public List<Song> getAllSongsByTitle(String title);
+
+    public List<Song> findAllSongsFromArtist(String artist);
+
+    public List<Song> findAllSongsFromArtistById(int ArtistID);
+
+    public List<Song> findAllFromAlbumByName(String albumName);
+
+    public List<Song> findAllFromAlbumById(int albumId);
+
+    public List<Song> getSongsInPlaylistByPlaylistName(String name);
+
 }
